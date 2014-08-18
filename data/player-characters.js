b69e4dffd47c8bdc93f74166f32005a5e633ecc8
Zach Bennett <xslingshot@gmail.com>
2014-08-18 19:29:24 -0400

    Add better comments to gamepad input in database.
    Signed-off-by: Zach Bennett <xslingshot@gmail.com>

--- data/player-characters.js
+++ data/player-characters.js
@@ -13,12 +13,12 @@ function () {
 					"secondaryAttack": "right_mouse_button"
 				},
 				gamepad: {
-					"move": "left", // Assumes joystick
-					"primaryAttack": "right", // Assumes joystick
-					"secondaryAttack": 0,
-					"debug1": 1,
-					"debug2": 2,
-					"debug3": 3
+					"move": "left",				// Assumes joystick
+					"primaryAttack": "right",	// Assumes joystick
+					"secondaryAttack": 0,		// A
+					"debug1": 1,				// B
+					"debug2": 2,				// X
+					"debug3": 3					// Y
 				}
 			}
 		}
