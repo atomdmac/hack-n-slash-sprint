980d7c81c30ea2f41feb1bd531a342c3aa84acd9
suicidepills <atom.mac@gmail.com>
2014-09-12 17:05:12 -0400

    Add wander/seek behaviors to NPCs.
    NPCs will only pursue the player if they are within line of sight.
    
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/entities/npc.js
+++ js/entities/npc.js
@@ -8,7 +8,10 @@ function NPC (options) {
 
 	this.isDistracted = false;
 	this.distractionRate = options.distractionRate;
-	this.courseOfAction = {};
+	this.courseOfAction = {
+		move: {angle: 0, magnitude: 0},
+		primaryAttack: {}
+	};
 }
 
 NPC.prototype = new Character({});
@@ -32,55 +35,87 @@ NPC.prototype.rollForDistraction = function(distractionRateMultiplier) {
 };
 
 NPC.prototype.decideNextAction = function() {
-	// Hunt Player 0.
-	if (this.resources.health > 0) {
-		this.rollForDistraction();
-		if (this.isDistracted) {
-			// Find angle to player.
-			var p1 = {
-				x: this.x,
-				y: this.y
-			};
-			var p2 = {
-				x: this._gameData.players[0].x,
-				y: this._gameData.players[0].y
-			};
-			var analogX = p2.x - p1.x;
-			var analogY = p2.y - p1.y;
-			
-			var angleToPlayer = Math.atan2(analogX, analogY);
-			
-			this.courseOfAction.move = {
-				angle: angleToPlayer,
-				magnitude: 0.2
-			};
-			
-			
-			var reach = 50;
-			var startX = this.x;
-			var startY = this.y;
-			var endX = startX + reach * Math.sin(angleToPlayer);
-			var endY = startY + reach * Math.cos(angleToPlayer);
-			
-			
-			this.courseOfAction.primaryAttack = {
-				reach : reach,
-				startX: startX,
-				startY: startY,
-				endX  : endX,
-				endY  : endY,
-				angle : angleToPlayer
-			};
-		}
-		if (this.courseOfAction.move) {
-			this.move(this.courseOfAction.move.angle,
-					  this.courseOfAction.move.magnitude);
-		}
-		if (this.courseOfAction.primaryAttack) {
-			this.primaryAttack(this.courseOfAction.primaryAttack);
-		}
+	// Do nothing if dead.
+	if (this.resources.health > 0 === false) return;
+
+	var player   = this._gameData.players[0],
+		// Round position values to prevent lineOfSight from breaking.
+		startPos = [Math.round(this.x)  , Math.round(this.y)],
+		endPos   = [Math.round(player.x), Math.round(player.y)],
+		collisionLayer = this._gameData.layers.collision;
+
+	if(collisionLayer.lineOfSight(startPos, endPos)) {
+		this.seek();
+	} else {
+		this.wander();
 	}
+
+	// This currently causes all character health to drop to 0 as soon as the
+	// game starts.
+	/*
+	if (this.courseOfAction.primaryAttack) {
+		this.primaryAttack(this.courseOfAction.primaryAttack);
+	}
+	*/
 	
+};
+
+NPC.prototype.seek = function () {
+	// TODO: Allow NPCs to seek characters/locations besides the player.
+	// Find angle to player.
+	var p1 = {
+		x: this.x,
+		y: this.y
+	};
+	var p2 = {
+		x: this._gameData.players[0].x,
+		y: this._gameData.players[0].y
+	};
+	var analogX = p2.x - p1.x;
+	var analogY = p2.y - p1.y;
+	
+	var angleToPlayer = Math.atan2(analogX, analogY);
+	
+	this.courseOfAction.move = {
+		angle: angleToPlayer,
+		magnitude: 0.8
+	};
+	
+	
+	var reach = 50;
+	var startX = this.x;
+	var startY = this.y;
+	var endX = startX + reach * Math.sin(angleToPlayer);
+	var endY = startY + reach * Math.cos(angleToPlayer);
+	
+	
+	this.courseOfAction.primaryAttack = {
+		reach : reach,
+		startX: startX,
+		startY: startY,
+		endX  : endX,
+		endY  : endY,
+		angle : angleToPlayer
+	};
+
+	this.move(this.courseOfAction.move.angle,
+					  this.courseOfAction.move.magnitude);
+};
+
+NPC.prototype.wander = function () {
+	this.rollForDistraction();
+	if (this.isDistracted) {
+		// Decide how to move in the X-axis.
+		var analogX = Math.round(Math.random()) ? Math.random() : Math.random() * -1;
+		var analogY = Math.round(Math.random()) ? Math.random() : Math.random() * -1;
+		
+		this.courseOfAction.move.angle = Math.atan2(analogX, analogY);
+		this.courseOfAction.move.magnitude = Math.sqrt(analogX*analogX+analogY*analogY);
+	}
+	if(this.courseOfAction.move) {
+		this.move(this.courseOfAction.move.angle,
+						  this.courseOfAction.move.magnitude);
+	}
 };
 
 NPC.prototype.kill = function() {
